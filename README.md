97ab550e0de9fc0a6d29573d0e37e43696284bd2
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-07-22 00:16:29 +0200

    Allow running all recipes from the command line

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Requirements:
 - [Wack0's DingusPPC binaries](https://github.com/Wack0/dingusppc-nt/releases) in dingusppc/
 - disabled DPI scaling for dingusppc.exe in compatibility properties
 
-```
-python smyguscpp.py path\to\cmake\project
+```powershell
+# Configure and build
+python smyguscpp.py cmake.all path\to\cmake\project
+
+# Alternatively
+python smyguscpp.py cmake.configure path\to\cmake\project
+python smyguscpp.py cmake.build
+
+# Just boot Windows NT
+python smyguscpp.py nt.boot path\to\iso\image
 ```
--- smyguscpp.py
+++ smyguscpp.py
@@ -1,5 +1,20 @@
+import importlib
+import inspect
+import pkgutil
+import recipes
 import sys
 
-from recipes import cmake
+if 1 < len(sys.argv):
+    recipe_name, func_name = sys.argv[1].split('.')
+    recipe = importlib.import_module(f'recipes.{recipe_name}')
+    func = getattr(recipe, func_name)
 
-cmake.all(sys.argv[1])
+    func(*sys.argv[2:])
+    exit()
+
+for module in pkgutil.iter_modules(recipes.__path__):
+    print(module.name)
+    recipe = importlib.import_module(f'recipes.{module.name}')
+    for member in inspect.getmembers(recipe):
+        if inspect.isfunction(member[1]):
+            print(f'\t{member[0]}')
